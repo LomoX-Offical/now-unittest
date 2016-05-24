205afdb177d51a77184bd8c9d01aa995f7d63c7b
zqzjz0911 <zqzjz1982@gmail.com>
2016-05-24 15:47:46 +0800

    fix some bug for unitest.py
    Signed-off-by: zqzjz0911 <zqzjz1982@gmail.com>

--- unitest.py
+++ unitest.py
@@ -8,43 +8,52 @@ import smtplib
 import subprocess
 import psutil
 
-def walk_dir(dir,fileinfo,topdown=True):
+def walk_dir(dir, py_path, topdown = True):
+    txt = '----------------------------------------\n';
+    txt += 'unit test result:\n';
+
+    logpath = os.path.abspath(py_path);
+    logfile = '%s\\result.log' % (logpath);
+    log = open(logfile, 'w');
+    log.write(txt);
+    log.close();
+
     for root, dirs, files in os.walk(dir, topdown):
         for name in files:
-            if name.endswith('.t'):
-                print(os.path.join(name))
-                logpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-                
-                killed = []
-                for proc in psutil.process_iter():
-                    if proc.name == 'nginx.exe' and proc.pid != os.getpid():
-                        proc.kill()
-                        killed.append(proc.pid)
-                time.sleep(5)
-                
-                os.system('perl %s > %s\\1.log'%(os.path.join(name),logpath))                
-                f=open('%s\\1.log'%logpath,'r+')
-                ftxt=os.path.join(name) + '\n' + f.read()
-                cmd_line = 'perl %s'%os.path.join(name)
-                process = subprocess.Popen(args=cmd_line, stderr=subprocess.PIPE)
-##                print process.stderr.read();          
-                htmlText = process.stderr.read();
-                ftxt += htmlText
-                log= open('%s\\nginx.log'%logpath,'a')
-                log.write(ftxt)
-        for name in dirs:
-##            print(os.path.join(name))
-            fileinfo.write('  ' + os.path.join(root,name) + '\n')
-    f.close()
-    ftxt1='----------------------------------------'
-    ftxt2='来自Auto_Test_Ent机器人助手NginxTest的消息'
-    ftxt1=ftxt1+'\n'+ftxt2
-    log.write(ftxt1)
-    log.close()
-if __name__ == '__main__' :
-    os.chdir('.\\unit-tests')
-    fileinfo = open('..\\list2.txt','w')
-    walk_dir(os.getcwd(),fileinfo)
-    fileinfo.close()
+            if name.endswith('.t') == False:
+                continue;
 
- 
+            print(os.path.join(name));
+
+            killed = [];
+            for proc in psutil.process_iter():
+                if proc.name == 'nginx.exe' and proc.pid != os.getpid():
+                    proc.kill();
+                    killed.append(proc.pid);
+            time.sleep(1);
+
+            cmd_line = 'perl %s' % (os.path.join(name));
+
+            process = subprocess.Popen(args = cmd_line, stderr = subprocess.PIPE, stdout = subprocess.PIPE);
+            out, err = process.communicate();
+
+            txt = os.path.join(name) + '\nout:\n' + out;
+            txt += '\nerror:\n' + err;
+
+            log = open(logfile, 'a');
+            log.write(txt);
+            log.close();
+
+        for name in dirs:
+            print(os.path.join(root, name));
+
+if __name__ == '__main__' :
+    if len(sys.argv) < 1 :
+        print "usage : python unitest.py nginx_exe_path";
+        sys.exit(0);
+
+    py_path = os.getcwd();
+    os.environ['TEST_NGINX_BINARY'] = sys.argv[1];
+    os.chdir('.\\nginx-tests');
+    walk_dir(os.getcwd(), py_path);
+
